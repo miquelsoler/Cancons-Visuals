39b39fe68bb07c8fbf3e95d8202763fe970c98d7
Miquel Soler <miquelangelsoler@gmail.com>
2015-10-19 18:26:33 +0200

    Squashed 'src/External/PMAudioAnalysis/' changes from f31944b..c6ddb83
    c6ddb83 Working on mel bands.
    2f4dc58 Minor changes
    
    git-subtree-dir: src/External/PMAudioAnalysis
    git-subtree-split: c6ddb8307ab0d9e40fd6cf13f5520feb844a938c

--- PMAudioOutParams.h
+++ PMAudioOutParams.h
@@ -8,6 +8,8 @@
 #ifndef PMAudioOutParams_h
 #define PMAudioOutParams_h
 
+#pragma once
+
 struct baseAudioOutParams
 {
     int deviceID;
@@ -25,11 +27,9 @@ struct onsetParams : public baseAudioOutParams
 {
 };
 
-struct fftBandsParams : public baseAudioOutParams
+struct freqBandsParams : public baseAudioOutParams
 {
-    float low;
-    float mid;
-    float high;
+    float *melBands;
 };
 
 
