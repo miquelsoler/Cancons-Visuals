5fd29ce6b93a780db1dd3e6836fc468095f64e94
eduardfrigola <eduardfrigola@yahoo.com>
2015-10-19 23:14:20 +0200

    added motion extractor

---
+++ src/Motion/PMMotionExtractor.cpp
@@ -0,0 +1,9 @@
+//
+//  PMMotionExtractor.cpp
+//  PMCancons_Visuals
+//
+//  Created by Eduard Frigola on 16/10/15.
+//
+//
+
+#include "PMMotionExtractor.hpp"
---
+++ src/Motion/PMMotionExtractor.hpp
@@ -0,0 +1,14 @@
+//
+//  PMMotionExtractor.hpp
+//  PMCancons_Visuals
+//
+//  Created by Eduard Frigola on 16/10/15.
+//
+//
+
+#ifndef PMMotionExtractor_hpp
+#define PMMotionExtractor_hpp
+
+#include <stdio.h>
+
+#endif /* PMMotionExtractor_hpp */
