b3298f80007cf13c395caec99425388085f72ad3
eduardfrigola <eduardfrigola@yahoo.com>
2015-11-30 18:36:51 +0100

    Added Base Renderer

---
+++ src/Renderer/PMBaseRenderer.cpp
@@ -0,0 +1,9 @@
+//
+//  PMBaseRenderer.cpp
+//  PMCancons_Visuals
+//
+//  Created by Eduard Frigola on 30/11/15.
+//
+//
+
+#include "PMBaseRenderer.hpp"
---
+++ src/Renderer/PMBaseRenderer.hpp
@@ -0,0 +1,45 @@
+//
+//  PMBaseRenderer.hpp
+//  PMCancons_Visuals
+//
+//  Created by Eduard Frigola on 30/11/15.
+//
+//
+
+#ifndef PMBaseRenderer_hpp
+#define PMBaseRenderer_hpp
+
+#include "ofMain.h"
+#include "PMAudioAnalyzer.hpp"
+
+class PMBaseRenderer
+{
+public:
+    PMBaseRenderer(int layer){};
+    virtual void setup();
+    virtual void update();
+    virtual void draw();
+    virtual void clear();
+    
+    virtual void drawIntoFBO() = 0;
+    
+    ofFbo*      getFbo(){return &fbo;};
+    int         getLayer(){return layer;};
+    
+    void showGUI(bool show);
+    
+    // changed ...
+    virtual void pitchChanged(pitchParams pitchParams);
+    virtual void energyChanged(energyParams energyParams);
+    virtual void silenceStateChanged(silenceParams &silenceParams);
+    virtual void pauseStateChanged(pauseParams &pauseParams);
+    
+protected:
+    int         layer;
+    ofFbo       fbo;
+    
+};
+
+#endif /* PMBaseRenderer_hpp */
+
+
