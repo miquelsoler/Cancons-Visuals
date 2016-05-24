95bbb47a854ecef16b136b0d52436b6340d68202
Eduard Frigola <eduardfrigola@gmail.com>
2016-05-25 00:43:20 +0200

    Fix for timeStampString formating in windows

--- src/Scenes/PMSc10Thanks.cpp
+++ src/Scenes/PMSc10Thanks.cpp
@@ -31,8 +31,7 @@ void PMSc10Thanks::setup()
     //Primer ha de pillar el nom, sino No es pot generar l'fbo
     songName = PMSongSelector::getInstance().getSongname();
     userName = "Interpretat per " + PMSharedSettings::getInstance().getUserName();
-    //dateName = ofGetTimestampString("%d/%m/%Y, %H%:%M");
-	dateName = "12/12/16, 23:59";
+    dateName = ofGetTimestampString("%d/%m/%Y, %H:%M");
     //carrega la imatge, alloca el fbo i genera fbo
     ofClear(0, 0, 0);
     printImage.load("TempRender.png");
